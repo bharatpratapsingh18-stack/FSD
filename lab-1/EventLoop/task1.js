2bff3f7aa97ced80c70dbf7301615c7561c6ab4a
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-07-28 09:01:39 +0530

    task

--- lab-1/EventLoop/task1.js
+++ lab-1/EventLoop/task1.js
@@ -10,14 +10,23 @@ const f2 = () => {
   console.log("f2 running");
   console.log("f2 ends");
 };
+
 const f3 = () => {
   console.log("f3 starts");
   console.log("f3 running");
   console.log("f3 end");
-};
+};    
 function main() {
   console.log("main");
-  f1();
+  f1(); 
   console.log("end main");
 }
 main();
+//the calling process which we used here is called synchronous call.
+//it is single threaded => it only perform onr task at a time.
+// in asynchronous we use event loop to manage the call stack.
+// asynchronous call using timers-
+//1.set Timeout 
+//2.set immediate
+//3. process.nexttick
+//4.set internal
