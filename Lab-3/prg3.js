1899b3dabb5c5bd69cc34228a979ec708e4aec4f
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-08-20 14:50:11 +0530

    prg3

---
+++ Lab-3/prg3.js
@@ -0,0 +1,28 @@
+import http from 'http'
+
+const server = http.createServer((req,res)=>{
+    if(http.url=='/'){
+        res.end('<h1>Home Page</h1>');
+    }
+    else if(http.url=='/about'){
+        res.end("<h2>About us Page</h2>");
+    }
+    else if(http.url=='/product'){
+        res.end(`<h1>Mobile Phone</h1>
+                 <h2>Price: 25000</h2>
+                 <p>Discout: 5%</p>
+                 <a href='#'>Buy Now</a>
+                 `);
+    }
+    else {
+        res.statusCode = 404;
+        res.end(`
+            <h1>404, Not found</h1>
+            <p>Page Not found <p>
+
+            `)
+    }
+    console.log("welcome to the club");
+})
+
+server.listen(4444,()=> console.log("Server is intitalizing"));
