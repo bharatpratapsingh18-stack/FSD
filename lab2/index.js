495c9432641bf33bd81f32548ee4fac876843ceb
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-03 15:02:52 +0530

    read files

--- lab2/index.js
+++ lab2/index.js
@@ -11,7 +11,7 @@ const addContent = async (fname, content) => {
     console.log(`${content} written in file: ${fname}`);
 };
 
-const readContent = async (fname) => {
+const readContent = async (fname) => {  
     const data = await readFile(fname, "utf-8");
     return data;
 };
